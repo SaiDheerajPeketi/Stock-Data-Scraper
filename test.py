c6edd6b76fe242ff788bd8c79fc6367c751fddd9
SaiDheerajPeketi <saidheerajpeketi@gmail.com>
2023-05-05 12:07:09 +0530

    Add Tester

---
+++ test.py
@@ -0,0 +1,11 @@
+#Code to test if we have permission to scrape a website
+
+import requests
+
+from bs4 import BeautifulSoup
+
+url = input("Enter the URL of the website: ")
+
+r = requests.get(url)
+
+print(r.status_code)
